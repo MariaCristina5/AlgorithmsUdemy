4f68593b2508c1a5b125fc46c8f90be9d8a65f1c
MariaCristina5 <113431422+MariaCristina5@users.noreply.github.com>
2022-12-16 14:07:44 +0200

    Update dijkstrasAlgo.js

--- dijkstrasAlgo.js
+++ dijkstrasAlgo.js
@@ -72,7 +72,28 @@ class WeightedGraph {
                 }
             }
         }
-        return path.concat(smallest).reverse;
+        return path.concat(smallest).reverse();
     }
 }
 
+var graph = new WeightedGraph()
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A","B", 4);
+graph.addEdge("A","C", 2);
+graph.addEdge("B","E", 3);
+graph.addEdge("C","D", 2);
+graph.addEdge("C","F", 4);
+graph.addEdge("D","E", 3);
+graph.addEdge("D","F", 1);
+graph.addEdge("E","F", 1);
+
+
+graph.Dijkstra("A", "E");
+
+// ["A", "C", "D", "F", "E"]
